b477322bb84e517bf437e38c484b24a991bfb768
fau77 <you@example.com>
2018-10-24 18:54:57 +0300

    day 1

---
+++ Assets/_My_Unity_Project/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnObject;
+	public float spawnTime = 1f;
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			Instantiate (spawnObject, transform.position, transform.rotation);
+			timer = spawnTime;
+		}
+	}
+}
