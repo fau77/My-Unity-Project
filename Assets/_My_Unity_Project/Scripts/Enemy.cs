816288f5cbe7e23e01cd06108181632a8df787ba
fau77-home-PK <fau77@mail.ru>
2018-10-28 15:00:41 +0300

    AR start

---
+++ Assets/_My_Unity_Project/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 2f;
+
+    private Transform waypoints;
+    private Transform waypoint;
+
+    private int waypointIndex = -1;
+
+    // Use this for initialization
+    void Start () {
+        waypoints = GameObject.Find("WayPoints").transform;
+        NextWaypoint();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = waypoint.transform.position - transform.position;
+        dir.y = 0;
+
+        float _speed = Time.deltaTime * speed;
+        transform.Translate(dir.normalized * _speed);
+        if (dir.magnitude <= _speed)
+            NextWaypoint();
+    }
+
+    void NextWaypoint()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.childCount)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypoint = waypoints.GetChild(waypointIndex);
+    }
+}
