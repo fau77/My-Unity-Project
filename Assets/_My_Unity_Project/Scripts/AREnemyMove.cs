8aca14385a64d0d306ef6cd2c34c11c25add36d2
fau77-home-PK <fau77@mail.ru>
2018-10-31 22:57:27 +0300

    AR 3.1

--- Assets/_My_Unity_Project/Scripts/AREnemyMove.cs
+++ Assets/_My_Unity_Project/Scripts/AREnemyMove.cs
@@ -1,31 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Vuforia;
 
 public class AREnemyMove : MonoBehaviour, ITrackableEventHandler
-                                                
-    {
+
+{
     public GameObject Enemy;
     public GameObject ImageTargetPath1;
+    public GameObject ImageTargetTower1;
     public float speed = 0.2f;
 
-    private TrackableBehaviour mTrackableBehaviour;
+    private TrackableBehaviour tb_Path1;
+    private TrackableBehaviour tb_Tower1;
+
     private Transform tr_Enemy;
     private Transform tr_Path1;
 
     void Start()
+    {
+        tb_Path1 = ImageTargetPath1.GetComponent<TrackableBehaviour>();
+        if (tb_Path1)
         {
-            mTrackableBehaviour = ImageTargetPath1.GetComponent<TrackableBehaviour>();
-            if (mTrackableBehaviour)
-            {
-                mTrackableBehaviour.RegisterTrackableEventHandler(this);
-            }
+            tb_Path1.RegisterTrackableEventHandler(this);
+        }
 
-            tr_Enemy = GameObject.Find("Enemy").transform;
-            tr_Path1 = GameObject.Find("ImageTargetPath1").transform;
+        tb_Tower1 = ImageTargetTower1.GetComponent<TrackableBehaviour>();
+        if (tb_Tower1)
+        {
+            tb_Tower1.RegisterTrackableEventHandler(this);
+        }
+
+        tr_Enemy = GameObject.Find("Enemy").transform;
+        tr_Path1 = GameObject.Find("ImageTargetPath1").transform;
     }
 
-        void Update()
-        {
+    void Update()
+    {
         //Vector3 dir = tr_Path1.transform.position - tr_Enemy.transform.position;
 
         //float _speed = Time.deltaTime * speed;
@@ -33,26 +44,53 @@ public class AREnemyMove : MonoBehaviour, ITrackableEventHandler
         //if (dir.magnitude <= _speed) { tr_Enemy.transform.Translate(dir.normalized); };
         //tr_Enemy.transform.position = tr_Path1.transform.position;
         //Debug.Log(tr_Enemy.transform.position);
+
+        // Get the Vuforia StateManager
+        //StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        //IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        // Iterate through the list of active trackables
+        //Debug.Log("List of trackables currently active (tracked): ");
+        //foreach (TrackableBehaviour tb in activeTrackables)
+        //{
+        //    Debug.Log("Trackable: " + tb.TrackableName);
+        //}
     }
 
     public void OnTrackableStateChanged(
                                     TrackableBehaviour.Status previousStatus,
                                     TrackableBehaviour.Status newStatus)
     {
-        if (newStatus == TrackableBehaviour.Status.DETECTED ||
-            newStatus == TrackableBehaviour.Status.TRACKED ||
-            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-        {
-        // Play when target is found
-        Enemy.GetComponent<Animator>().SetTrigger("Run");
-
-        }
-        else
-        {
-       //Stop play when target is lost
-        Enemy.GetComponent<Animator>().SetTrigger("Idle");
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
         
+        Debug.Log("Before foreach: " + newStatus);
+        
+
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            Debug.Log("Before check: " + tb.TrackableName);
+            Debug.Log("Status: " + newStatus);
+            //if (tb.TrackableName == "Tower3")
+            //{
+            //    if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            //   newStatus == TrackableBehaviour.Status.TRACKED ||
+            //    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            //    {
+            //        // Play when target is found
+            //        Enemy.GetComponent<Animator>().SetTrigger("Run");
+            //    }
+            //    else
+            //    {
+            //        //Stop play when target is lost
+            //        Enemy.GetComponent<Animator>().SetTrigger("Idle");
+            //    }
+            //}
         }
     }
-
 }
+
