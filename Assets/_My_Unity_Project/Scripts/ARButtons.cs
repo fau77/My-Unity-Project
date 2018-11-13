83ed2ccfdc3c9111cd4399d342e152fab661d7d5
fau77-work <you@example.com>
2018-11-13 17:32:18 +0300

    AR buttos add struct

--- Assets/_My_Unity_Project/Scripts/ARButtons.cs
+++ Assets/_My_Unity_Project/Scripts/ARButtons.cs
@@ -1,28 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
 public class ARButtons : MonoBehaviour {
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject MenuText;
+    [SerializeField] public GameTargets[] sf_Targets;
+    [Serializable]
+    public struct GameTargets
+    {
+        public string Name;
+        public GameObject Target;
+    }
 
     public void ARAlign()
     {
         HashSet<string> hsTargetNamesMust = new HashSet<string>();
-        hsTargetNamesMust.Add("Cave");
-        hsTargetNamesMust.Add("Tower1");
-        hsTargetNamesMust.Add("Enemy");
-
-        //GameObject MenuText = GameObject.Find("MenuText");
+        //hsTargetNamesMust.Add("Cave");
+        //hsTargetNamesMust.Add("Tower1");
+        //hsTargetNamesMust.Add("Enemy");
+        foreach (var el in sf_Targets)
+        {
+            hsTargetNamesMust.Add(el.Name);
+        }
 
         HashSet<string> hsTargetNamesHave = new HashSet<string>();
         
@@ -43,16 +45,24 @@ public class ARButtons : MonoBehaviour {
         hsTargetNamesMust.ExceptWith(hsTargetNamesHave);
         if (hsTargetNamesMust.Count > 0)
         {
-            //MenuText.SetActive(true);
-            GUI.Label(new Rect(0, 0, 120, 100), "Предметов собрано");
+            MenuText.SetActive(true);
         }
-
+        else
+        {
+            MenuText.SetActive(false);
+            Debug.Log("All targets ready!");
+        foreach (var el in sf_Targets)
+            {
+                Debug.Log(el.Name + ": " + el.Target.transform.position);
+            }
+        }
         hsTargetNamesMust.Clear();
         hsTargetNamesHave.Clear();
     }
 
     public void ARStart()
     {
+        sf_Targets[0].Target.GetComponent<Animator>().SetTrigger("Run");
 
     }
 }
